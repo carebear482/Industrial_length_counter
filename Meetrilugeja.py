23e160962aaa20af42b20e72f6a2336090a6c1e3
carebear482 <osuuster@gmail.com>
2019-09-27 15:14:48 +0300

    Initial test.

--- Meetrilugeja.py
+++ Meetrilugeja.py
@@ -7,7 +7,10 @@ if using_raspberry:
 
     # Setup the GPIOs as inputs with Pull Ups since the buttons are connected to GND
     GPIO.setmode(GPIO.BCM)
-    physical_connection = {'meter_pulse': 26, 'reset_pulse': 19, 'alarm': 13, 'close_ui': 6, 'timer_mode': 5}
+    physical_connection = {'meter_pulse': 26,
+                           'reset_pulse': 19,
+                           'alarm': 13,
+                           'close_ui': 6}
     # Define input pull ups
     for i in physical_connection.values():
         GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -22,8 +25,6 @@ width = 1920
 height = 1080
 pygame.init()
 pygame.mouse.set_visible(False)
-font_small = pygame.font.Font(None, 150)
-font_big = pygame.font.Font(None, 400)
 lcd = pygame.display.set_mode([width, height], pygame.FULLSCREEN)
 color = {'white': (255, 255, 255),
          'black': (0, 0, 0),
@@ -31,6 +32,7 @@ color = {'white': (255, 255, 255),
          'green': (0, 150, 0)}
 
 # Initial counter values
+# Drum mode variables
 current_drum = 0
 last_drum = 0
 meter_debounce_bit = 0
@@ -39,144 +41,144 @@ done = False
 meter_pulse = 0
 alarm = 0
 reset = 0
+
+#Timer mode variables
 start_time = None
-timer_second = 0
-timer_minute = 0
-timer_hour = 0
-timer_total_minutes = 0
-timer_setpoint_minutes = ''
-timer_completion_percent = 0
-timer_color_bar = 0
+timer_total_second = 0
+timer_setpoint = ''
 using_as_counter = 0
-using_as_timer = 1
 start = False
 word = ''
 last_drum_time = 0
-setpoint_seconds = 0
-seconds_left = 0
+
+setpoint_second = 0
+setpoint_minute = 0
+setpoint_hour = 0
+
+timer_setpoint_in_seconds = 0
+
+time_left_hour = 0
+time_left_minute = 0
+time_left_seconds = 0
+
 # Working loop
 while not done:
-
-    # Loop variables:
+    # Simulation loop for capturing keyboard inputs
     meter_pulse = 0
     reset = 0
-
-    # Simulation loop for capturing keyboard inputs
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN and not event.unicode == '':
             if event.unicode == 'j':
                 print(event.unicode)
                 done = True
             if not alarm and event.unicode == 'a':
                 print(event.unicode)
                 alarm = True
-
             elif alarm and event.unicode == 'a':
                 alarm = False
-
             if event.unicode == 'm':
                 meter_pulse = True
-
+                print(event.unicode)
             if event.unicode == 'r':
                 reset = True
-
-            if event.unicode == 's' and not timer_setpoint_minutes == '':
+            if event.unicode == 's' and not timer_setpoint == '':
                 start = True
                 start_time = pygame.time.get_ticks()
-                timer_second = 0
-                timer_minute = 0
-                timer_hour = 0
-                timer_total_minutes = 0
-                last_drum_time = timer_total_minutes
+                timer_total_second = 0
             # Capture and add together user input as strings.
             if event.unicode.isdigit():
-                if word == 0:
-                    word = ''
                 word += event.unicode
-
+                timer_setpoint = int(word)
+                if timer_setpoint:
+                    setpoint_hour = timer_setpoint / 10000
+                    setpoint_minute = (setpoint_hour - int(setpoint_hour)) * 100
+                    setpoint_second = (setpoint_minute - int(setpoint_minute)) * 100
             # Press enter to accept the setpoint as int and reset user input
-            if event.key == pygame.K_RETURN and not start and word :
-                if word.isdigit():
-                    #Convert user input as future setpoint
-                    timer_setpoint_minutes = int(word)
-                    setpoint_seconds = timer_setpoint_minutes * 60
-                    setpoint_hours = timer_setpoint_minutes / 60
-                    setpoint_minutes = timer_setpoint_minutes - int(setpoint_hours) * 60
-                    print('new setpoint set!', int(setpoint_hours),'hours', int(setpoint_minutes),'min', setpoint_seconds, 'sec')
-                    word = ''
-
-    # Avoid exception by setting empty string to 0
-    if not word:
-        word = 0
+            if event.key == pygame.K_RETURN and not start and word.isdigit():
+                # Convert user input as future setpoint
+                timer_setpoint_in_seconds = int(setpoint_hour) * 3600 + int(setpoint_minute) * 60 + int(
+                    setpoint_second)
+                word = ''
 
     # Stop counting when setpoint has reached
-    if timer_setpoint_minutes == timer_total_minutes:
+    if timer_setpoint_in_seconds == int(timer_total_second):
         start_time = None
         start = 0
 
     if start_time:
-        timer_second = (pygame.time.get_ticks() - start_time) / 100
-    if setpoint_seconds > 0:
-        seconds_left = timer_second - setpoint_seconds
-    if int(seconds_left) >= 60:
-        start_time = pygame.time.get_ticks()
-        timer_second = 0
-        timer_minute += 1
-        timer_total_minutes += 1
-    if timer_minute >= 60:
-        timer_minute = 0
-        timer_hour += 1
+        timer_total_second = int((pygame.time.get_ticks() - start_time) / 1000)
+    if start:
+        # Finding the time left
+        total_time_left_seconds = int(timer_setpoint_in_seconds - timer_total_second)
+        time_left_hour = total_time_left_seconds / 3600
+        time_left_minute = (time_left_hour - int(time_left_hour)) * 60
+        time_left_seconds = (time_left_minute - int(time_left_minute)) * 60
+    # Show setpoint in main screen
+    else:
+        time_left_hour = setpoint_hour
+        time_left_minute = setpoint_minute
+        time_left_seconds = setpoint_second
+
     # Add zeros if digit is single
-    if timer_hour < 10:
-        hours_to_screen = '0' + str(timer_hour)
+    if time_left_hour < 10:
+        hours_to_screen = '0' + str(int(time_left_hour))
     else:
-        hours_to_screen = str(timer_hour)
-    if timer_minute < 10:
-        minutes_to_screen = '0' + str(timer_minute)
+        hours_to_screen = str(int(time_left_hour))
+    if time_left_minute < 10:
+        minutes_to_screen = '0' + str(int(time_left_minute))
     else:
-        minutes_to_screen = str(timer_minute)
-    if timer_second < 10:
-        seconds_to_screen = '0' + str(int(timer_second))
+        minutes_to_screen = str(int(time_left_minute))
+    if time_left_seconds < 10:
+        seconds_to_screen = '0' + str(int(time_left_seconds))
     else:
-        seconds_to_screen = str(int(timer_second))
+        seconds_to_screen = str(int(time_left_seconds))
 
-    # Countdown
-
-    time = hours_to_screen + ':' + minutes_to_screen + ':' + seconds_to_screen
+    # Form classic clock look
+    if not start:
+        time = hours_to_screen + ':' + minutes_to_screen + ':' + seconds_to_screen
+        last_drum_time = time
+    else:
+        time = hours_to_screen + ':' + minutes_to_screen + ':' + seconds_to_screen
 
     # Paint background color to red in case of alarm
     if alarm:
         lcd.fill(color['red'])
-    elif timer_setpoint_minutes == timer_total_minutes:
+    elif timer_setpoint_in_seconds == int(timer_total_second) and not timer_setpoint_in_seconds == 0:
         lcd.fill(color['green'])
     else:
         lcd.fill(color['black'])
 
-
     # Current drum
     if using_as_counter:
-        text_big = font_big.render(current_drum, True, color['white'])
+        font_small = pygame.font.Font(None, 150)
+        font_big = pygame.font.Font(None, 800)
+
+        text_big = font_big.render('%d' %current_drum, True, color['white'])
         text_small_bottom = font_small.render('Viimane trummel  ''%d'' m' % last_drum, True, color['white'])
-        text_small_top = font_small.render('' '%d' % '', True, color['white'])
+        # Main textbox
+        text_box_big = text_big.get_rect(center=(width / 2, (height + height) / 4))
+        lcd.blit(text_big, text_box_big)
+
+        # Subbox
+        text_box_small = text_small_bottom.get_rect(center=(width / 2, (height - height / 10)))
+        lcd.blit(text_small_bottom, text_box_small)
     else:
-        text_small_top = font_small.render('Palun sisesta töö pikkus ' '%d' % int(word)  , True, color['white'])
+        font_small = pygame.font.Font(None, 150)
+        font_big = pygame.font.Font(None, 600)
         text_big = font_big.render(time, True, color['white'])
-        text_small_bottom = font_small.render('Viimane trummel oli ahjus ''%d'' tundi' % timer_total_minutes, True, color['white'])
-        if timer_setpoint_minutes and start:
-            text_small_top = font_small.render('Etteantud aeg ''%d'' ' % int(timer_setpoint_minutes), True, color['white'])
+        if not start:
+            text_small_bottom = font_small.render('Palun sisesta töö pikkus', True, color['white'])
+        else:
+            text_small_bottom = font_small.render('Etteantud aeg %s' % last_drum_time, True, color['white'])
+            # Main textbox
+        text_box_big = text_big.get_rect(midleft=(width / 16, (height + height) / 4))
+        lcd.blit(text_big, text_box_big)
 
+        # Subbox
+        text_box_small = text_small_bottom.get_rect(center=(width / 2, (height - height / 10)))
+        lcd.blit(text_small_bottom, text_box_small)
     # Set text sizes
-    # Main textbox
-    text_box_big = text_big.get_rect(center=(width / 2, (height + height) / 5))
-    lcd.blit(text_big, text_box_big)
 
-    # Subbox
-    text_box_small = text_small_bottom.get_rect(center=(width / 2, (height - height / 10)))
-    lcd.blit(text_small_bottom, text_box_small)
-
-    # Small box on top
-    text_box_small = text_small_bottom.get_rect(center=(width / 2, (height / 10)))
-    lcd.blit(text_small_top, text_box_small)
 
     pygame.display.update()
     # Meter counter
